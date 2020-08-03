63f13d6a2478edd3907976708f00c0a2c4e16ecd
fohlle <patte2000@hotmail.com>
2020-08-03 10:24:10 +0200

    complete

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,11 @@
 
 const container = document.querySelector("#container");
 
-function grid(num){
-for(let i = 0; i < num; i++){
+function grid(row, cell){
+    container.style.setProperty('--grid-rows', row);
+    container.style.setProperty('--grid-col', cell);
+
+for(let i = 0; i < row * cell; i++){
     const div = document.createElement("div");
     div.setAttribute('id', "grid");
     div.addEventListener("mouseover", () =>{
@@ -12,5 +15,22 @@ for(let i = 0; i < num; i++){
     container.appendChild(div);
 }
 }
+function removeDiv(){
+    while(container.firstChild){
+    container.removeChild(container.firstChild);
+    }
+}
 
-grid(256);
+
+function newGrid(){
+    let gridPrompt = prompt("how many grids do you want?");
+    grid(+gridPrompt,+gridPrompt);
+}
+
+const btn = document.querySelector("#clearGridBtn");
+btn.addEventListener("click", () => {
+    removeDiv();
+    newGrid();
+});
+
+grid(16, 16);
