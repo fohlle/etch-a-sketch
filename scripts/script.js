7b476907e27e015025e026f8f77a75919a97c86e
fohlle <patte2000@hotmail.com>
2020-08-03 10:40:00 +0200

    random colours

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ for(let i = 0; i < row * cell; i++){
     const div = document.createElement("div");
     div.setAttribute('id', "grid");
     div.addEventListener("mouseover", () =>{
-        div.setAttribute("style", "background:black");
+        div.setAttribute("style", randColor());
     });
     container.appendChild(div);
 }
@@ -19,6 +19,13 @@ function removeDiv(){
     while(container.firstChild){
     container.removeChild(container.firstChild);
     }
+}
+
+function randColor(){
+    const rand = Math.floor((Math.random() * 255) + 1);
+    const rand2 = Math.floor((Math.random() * 255) + 1);
+    const rand3 = Math.floor((Math.random() * 255) + 1);
+    return `background-color:rgb(${rand}, ${rand2},${rand3})`;
 }
 
 
